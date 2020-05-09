025e1084c69db147b1996a69c132e59c5e35c9df
Praveen <praveenislive@gmail.com>
2020-05-10 03:54:08 +0530

    45. Heptic Feedback Completed

--- DesignCode/Buttons.swift
+++ DesignCode/Buttons.swift
@@ -8,6 +8,14 @@
 
 import SwiftUI
 
+func hapticFeedback(type: UINotificationFeedbackGenerator.FeedbackType) {
+    UINotificationFeedbackGenerator().notificationOccurred(type)
+}
+
+func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    UIImpactFeedbackGenerator(style: style).impactOccurred()
+}
+
 struct Buttons: View {
     var body: some View {
         VStack(spacing: 50.0) {
@@ -74,11 +82,13 @@ struct RectangelButton: View {
             .gesture(
                 LongPressGesture(minimumDuration: 0.1, maximumDistance: 1).onChanged({ (value) in
                     self.tap = true
+                    impact(style: .heavy)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                         self.tap = false
                     }
                 }).onEnded({ (value) in
                     self.press.toggle()
+                    hapticFeedback(type: .success)
                 })
         )
     }
